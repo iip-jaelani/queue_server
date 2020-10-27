64b00930fe35cabac4572ff5a69905b9161dd4e9
lip jaelani <iip.jaelani@ur-hub.com>
2020-10-27 15:21:07 +0700

    redirect

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
+const { route } = require("./printer");
+
 var router = require("express").Router();
 
 router.use("/api/v1", require("./printer"));
-
+router.all("*", (req, res) =>
+	res.send({
+		error: true,
+		message: "What are you doing ?",
+	})
+);
 module.exports = router;
