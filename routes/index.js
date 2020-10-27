ce8e34888b8069b207f2b755955907696ed30dc6
lip jaelani <iip.jaelani@ur-hub.com>
2020-10-27 17:04:39 +0700

    printer

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const { route } = require("./printer");
 var router = require("express").Router();
 
 router.use("/api/v1", require("./printer"));
+router.use("/", (req, res) =>
+	res.send({
+		status: "200OK",
+	})
+);
 router.all("*", (req, res) =>
 	res.send({
 		error: true,
