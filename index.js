b7d83ad8e548ac4c2febe5fc5aa0bfa01cd3ca3a
lip jaelani <iip.jaelani@ur-hub.com>
2020-10-27 15:18:29 +0700

    queue serv

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+	bodyParser = require("body-parser");
+//INITIAL APP
+
+const app = express();
+//PORT
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+//ROUTES
+app.use(require("./routes"));
+//LISTING APPS
+app.listen(port, () => console.log("LISTEN ON PORT", port));
---
+++ routes/printer.js
@@ -0,0 +1,52 @@
+var router = require("express").Router();
+var Moment = require("moment");
+const ThermalPrinter = require("node-thermal-printer").printer;
+const PrinterTypes = require("node-thermal-printer").types;
+
+router.post("/print_queue", async (req, res) => {
+	const { serviceName, numberQueue, namePartner, host } = req.body;
+	const hostIp = host;
+	let printer = new ThermalPrinter({
+		type: PrinterTypes.EPSON,
+		interface: `tcp://${hostIp}:9100`, // Printer interface
+		lineCharacter: "-",
+		options: {
+			timeout: 5000,
+		},
+	});
+	printer.bold(true);
+	printer.alignCenter();
+	printer.setTextSize(1, 1);
+	printer.println(`${namePartner}`);
+	printer.newLine();
+	printer.newLine();
+	printer.println(`${serviceName}`);
+	printer.newLine();
+	printer.newLine();
+	printer.setTextSize(4, 4);
+	printer.println(`${numberQueue}`);
+	printer.newLine();
+	printer.newLine();
+	printer.setTextNormal();
+	printer.drawLine();
+	printer.println(
+		`Tanggal: ${Moment(new Date()).format("DD-MM-YYYY, h:mm:ss a")}`
+	);
+	printer.drawLine();
+	printer.cut();
+	try {
+		let execute = printer.execute();
+		res.send({
+			message: "success print data",
+			error: false,
+		});
+	} catch (error) {
+		res.send({
+			message: "failed print data",
+			error: true,
+		});
+		console.log("Print failed:", error);
+	}
+});
+
+module.exports = router;
